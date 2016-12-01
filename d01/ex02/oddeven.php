77f7a356d71150a5860c198dd870734fc512d1cb
wPef <f.demoncade@gmail.com>
2016-12-01 14:45:34 +0100

    handle exit

--- d01/ex02/oddeven.php
+++ d01/ex02/oddeven.php
@@ -1,7 +1,15 @@
 #!/usr/bin/php
 <?php
+
+$input = fopen("php://stdin", "r");
 while (42) {
-	$nb = readline("Entrez un nombre : ");
+	echo("Entrez un nombre: ");
+	$line = fgets($input);
+	if ($line == false) {
+		echo("\n");
+		exit();
+	}
+	$nb = substr($line, 0, -1);
 	if (is_numeric($nb)) {
 		print("Le chiffre ".$nb." est");
 		if ($nb % 2 == 0)
@@ -10,7 +18,7 @@ while (42) {
 			print(" Impair");
 	}
 	else
-		print($nb." n'est pas un chiffre");
+		print("'".$nb."'"." n'est pas un chiffre");
 	print("\n");
 }
 ?>
