9f1a55359b6f7b8275b859fe603d64a0e94be7d2
wPef <f.demoncade@gmail.com>
2016-12-15 15:29:19 +0100

    members_only

--- d03/members_only.php
+++ d03/members_only.php
@@ -7,22 +7,24 @@ $open = "<html><body>";
 $close = "</body></html>";
 
 
-header('WWW-Authenticate: Basic realm="Espace membres"');
 if (isset($_SERVER['PHP_AUTH_USER'])  and isset($_SERVER['PHP_AUTH_PW']))
 {
 	$user = $_SERVER['PHP_AUTH_USER'];
 	$pass = $_SERVER['PHP_AUTH_PW'];
-}
-if ($user === 'zaz' and $pass === 'jaimelespetitsponeys')
-{
-	$file = base64_encode(file_get_contents('img/42.png'));
-	echo $open."Bonjour ".ucfirst($user)."<br />";
-	echo "<img src='data:image/png;base64,$file'>";
-	echo $close;
+	if ($user === 'zaz' and $pass === 'jaimelespetitsponeys')
+	{
+		$file = base64_encode(file_get_contents('../img/42.png'));
+		echo $open."Bonjour ".ucfirst($user)."<br />";
+		echo "<img src='data:image/png;base64,$file'>";
+		echo $close;
+	}
+	else 
+		echo $open."Cette zone est accessible uniquement aux membres du site$close";
 }
 else
 {
+	header('WWW-Authenticate: Basic realm="Espace membres"');
 	header("HTTP/1.0 401 Unauthorized");
-	echo $open."Cette zone est accessible uniquement aux membres du site$close";
 }
+	exit;
 ?>
