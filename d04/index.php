7f6992bd38935a4c717e3403581f4197ab8cc9b8
wPef <f.demoncade@gmail.com>
2016-12-15 16:18:25 +0100

    d4e0 dont work on mamp ? localhost ne fonctionne pas

---
+++ d04/index.php
@@ -0,0 +1,26 @@
+<?php
+
+session_start();
+$form_h = '<form action="index.php" method="GET">
+	<p>Identifiant : <input type="text" name="login"'
+$form_closetag = '/></p>'
+$form_pass = '<p>Mot de passe : <input type="text" name="passwd" '
+$form_endsub = '<p><input type="submit" name="submit" value="OK"></p></form>';
+
+echo "<html><body>";
+if (!isset($_SESSION['login']))
+{
+	echo $form_h.$form_closetag.$form_pass.$form_closetag.$form_endsub;
+	$_SESSION['login'] = $_GET['login'];
+	$_SESSION['passwd'] = $_GET['passwd'];
+}
+else
+{
+	echo $form_h.' value="'.$_SESSION['login'].'"'.$form_closetag;
+	echo $form_pass.' value="'.$_SESSION['passwd'].'"'.$formclosetag;
+	echo $form_endsub;
+}
+var_dump($_SESSION);
+
+echo "</body></html>";
+?>
