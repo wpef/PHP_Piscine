abd8998556524626ab635eaddc2fbfcc9d8fcdaa
wPef <f.demoncade@gmail.com>
2017-01-08 21:15:54 +0100

    d4e0 ok

--- d04/index.php
+++ d04/index.php
@@ -1,13 +1,13 @@
 <?php
-
 session_start();
-$form_h = '<form action="index.php" method="GET">
-	<p>Identifiant : <input type="text" name="login"'
-$form_closetag = '/></p>'
-$form_pass = '<p>Mot de passe : <input type="text" name="passwd" '
+$form_h = '<form action="/Piscine/d04/index.php" method="GET">
+	<p>Identifiant : <input type="text" name="login"';
+$form_closetag = '/></p>';
+$form_pass = '<p>Mot de passe : <input type="text" name="passwd"';
 $form_endsub = '<p><input type="submit" name="submit" value="OK"></p></form>';
 
 echo "<html><body>";
+
 if (!isset($_SESSION['login']))
 {
 	echo $form_h.$form_closetag.$form_pass.$form_closetag.$form_endsub;
